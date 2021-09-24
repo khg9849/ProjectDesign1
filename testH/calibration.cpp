b861555d0e709ba0bc8230af49b9a00532401cb0
lacoon2874 <woakd0705@naver.com>
2021-09-24 04:00:19 -0700

    calibration code

--- testH/calibration.cpp
+++ testH/calibration.cpp
@@ -5,23 +5,11 @@ bool hwangCalibration::doCalibration(const char *pPath){
 
 	cv::glob(pPath, images);
 
-	vector<vector<cv::Point3f>> objpoints_left;
-	vector<vector<cv::Point3f>> objpoints_right;
-
-	vector<vector<cv::Point2f>> imgpoints_left;
-	vector<vector<cv::Point2f>> imgpoints_right;
-
-	vector<cv::Point3f> objp;
-
 	for(int i = 0; i < CHECKERBOARD[1]; i++){
 		for(int j = 0; j < CHECKERBOARD[0]; j++){
 			objp.push_back(cv::Point3f(j*23.6, i*23.6, 0));
 		}
 	}
-
-	cv::Mat frame, gray, pic[2];
-
-	vector<cv::Point2f> corner_pts;
 
 	for(int i = 0; i < images.size(); i++){
 		frame = cv::imread(images[i]);
@@ -33,7 +21,6 @@ bool hwangCalibration::doCalibration(const char *pPath){
 
 		for(int j = 0; j < 2; j++){
 			if(cv::findChessboardCorners(pic[j], cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE)){
-
 				cv::TermCriteria criteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.001);
 
 				cv::cornerSubPix(pic[j], corner_pts, cv::Size(11, 11), cv::Size(-1, -1), criteria);
@@ -50,24 +37,17 @@ bool hwangCalibration::doCalibration(const char *pPath){
 		}
 	}
 
-	cv::Mat cameraMatrix_left, distCoeffs_left, R_left, T_left;
-	cv::Mat cameraMatrix_right, distCoeffs_right, R_right, T_right;
-
 	cv::calibrateCamera(objpoints_left, imgpoints_left, cv::Size(pic[0].rows, pic[0].cols), cameraMatrix_left, distCoeffs_left, R_left, T_left);
 	cv::calibrateCamera(objpoints_right, imgpoints_right, cv::Size(pic[1].rows, pic[1].cols), cameraMatrix_right, distCoeffs_right, R_right, T_right);
 
-	cv::Mat R, T, E, F;
-
-	cv::Size imgsize = pic[0].size();
+	imgsize = pic[0].size();
 	
 	cv::stereoCalibrate(objpoints_left, imgpoints_left, imgpoints_right, cameraMatrix_left, distCoeffs_left, cameraMatrix_right, distCoeffs_right, imgsize, R, T, E, F, cv::CALIB_FIX_INTRINSIC, cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 1e-6));
 	
-	cv::Mat R1, R2, P1, P2, Q;
-	cv::Rect validRoi[2];
-	
 	cv::stereoRectify(cameraMatrix_left, distCoeffs_left, cameraMatrix_right, distCoeffs_right, imgsize, R, T, R1, R2, P1, P2, Q, cv::CALIB_ZERO_DISPARITY, 1, imgsize, &validRoi[0], &validRoi[1]);
 
-	cv::FileStorage fsw("calibrationData.xml", cv::FileStorage::WRITE);
+	cv::FileStorage fsw("calibration.xml", cv::FileStorage::WRITE);
+
 	fsw << "leftCameraMatrix" << cameraMatrix_left;
 	fsw << "leftCameraDistCoeff" << distCoeffs_left;
 	fsw << "rightCameraMatrix" << cameraMatrix_right;
@@ -86,23 +66,11 @@ bool hwangCalibration::doCalibration(const char *pPath){
 }
 
 bool hwangCalibration::doCalibration(vector<string> &imgList){
-	vector<vector<cv::Point3f>> objpoints_left;
-	vector<vector<cv::Point3f>> objpoints_right;
-
-	vector<vector<cv::Point2f>> imgpoints_left;
-	vector<vector<cv::Point2f>> imgpoints_right;
-
-	vector<cv::Point3f> objp;
-
 	for(int i = 0; i < CHECKERBOARD[1]; i++){
 		for(int j = 0; j < CHECKERBOARD[0]; j++){
 			objp.push_back(cv::Point3f(j*23.6, i*23.6, 0));
 		}
 	}
-
-	cv::Mat frame, gray, pic[2];
-
-	vector<cv::Point2f> corner_pts;
 
 	for(int i = 0; i < imgList.size(); i++){
 		frame = cv::imread(imgList[i]);
@@ -114,7 +82,6 @@ bool hwangCalibration::doCalibration(vector<string> &imgList){
 
 		for(int j = 0; j < 2; j++){
 			if(cv::findChessboardCorners(pic[j], cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE)){
-
 				cv::TermCriteria criteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.001);
 
 				cv::cornerSubPix(pic[j], corner_pts, cv::Size(11, 11), cv::Size(-1, -1), criteria);
@@ -131,24 +98,17 @@ bool hwangCalibration::doCalibration(vector<string> &imgList){
 		}
 	}
 
-	cv::Mat cameraMatrix_left, distCoeffs_left, R_left, T_left;
-	cv::Mat cameraMatrix_right, distCoeffs_right, R_right, T_right;
-
 	cv::calibrateCamera(objpoints_left, imgpoints_left, cv::Size(pic[0].rows, pic[0].cols), cameraMatrix_left, distCoeffs_left, R_left, T_left);
 	cv::calibrateCamera(objpoints_right, imgpoints_right, cv::Size(pic[1].rows, pic[1].cols), cameraMatrix_right, distCoeffs_right, R_right, T_right);
 
-	cv::Mat R, T, E, F;
-
-	cv::Size imgsize = pic[0].size();
+	imgsize = pic[0].size();
 	
 	cv::stereoCalibrate(objpoints_left, imgpoints_left, imgpoints_right, cameraMatrix_left, distCoeffs_left, cameraMatrix_right, distCoeffs_right, imgsize, R, T, E, F, cv::CALIB_FIX_INTRINSIC, cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 1e-6));
-	
-	cv::Mat R1, R2, P1, P2, Q;
-	cv::Rect validRoi[2];
-	
+
 	cv::stereoRectify(cameraMatrix_left, distCoeffs_left, cameraMatrix_right, distCoeffs_right, imgsize, R, T, R1, R2, P1, P2, Q, cv::CALIB_ZERO_DISPARITY, 1, imgsize, &validRoi[0], &validRoi[1]);
 
-	cv::FileStorage fsw("calibrationData.xml", cv::FileStorage::WRITE);
+	cv::FileStorage fsw("calibration.xml", cv::FileStorage::WRITE);
+
 	fsw << "leftCameraMatrix" << cameraMatrix_left;
 	fsw << "leftCameraDistCoeff" << distCoeffs_left;
 	fsw << "rightCameraMatrix" << cameraMatrix_right;
--- testH/includes/calibration.hpp
+++ testH/includes/calibration.hpp
@@ -3,9 +3,33 @@
 #include <opencv2/calib3d/calib3d_c.h>
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
 class hwangCalibration: public SYE::YeStereoCamera{
 	int CHECKERBOARD[2]{6,9};
+
+	vector<vector<cv::Point3f>> objpoints_left;
+	vector<vector<cv::Point3f>> objpoints_right;
+	
+	vector<vector<cv::Point2f>> imgpoints_left;
+	vector<vector<cv::Point2f>> imgpoints_right;
+
+	vector<cv::Point3f> objp;
+
+	cv::Mat frame, gray, pic[2];
+
+	vector<cv::Point2f> corner_pts;
+
+	cv::Mat cameraMatrix_left, distCoeffs_left, R_left, T_left;
+	cv::Mat cameraMatrix_right, distCoeffs_right, R_right, T_right;
+
+	cv::Mat R, T, E, F;
+
+	cv::Size imgsize;
+
+	cv::Mat R1, R2, P1, P2, Q;
+	cv::Rect validRoi[2];
 
 public:
 	virtual bool doCalibration(const char *pPath);
