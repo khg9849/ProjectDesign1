ae1a7cfec7a3f13c32c87ca7a2e94535c381f798
khg9849 <75154470+khg9849@users.noreply.github.com>
2021-09-25 02:25:44 +0900

    doCalibration
    do Calibration 구현
    yeStereoCamera.hpp를 include하는 calibration.hpp라는 헤더파일에 Calibration 클래스 선언. yeStereoCamera.cpp 파일에 Calibration 클래스 메소드 작성.
    임시 main.cpp파일 만들어 실행. calibration_result.xml파일 생성

--- src/yeStereoCamera.hpp
+++ src/yeStereoCamera.hpp
@@ -1,13 +1,15 @@
 #ifndef YESTEREOCAMERA_HPP
 #define YESTEREOCAMERA_HPP
-
 #include <vector>
 #include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/calib3d/calib3d_c.h>
 
 namespace SYE {
 
 class YeStereoCamera {
 private:
+	
 protected:
 	virtual bool doCalibration(const char *pPath) = 0;
 	virtual bool doCalibration(std::vector<std::string> &imgList) = 0;
@@ -16,6 +18,9 @@ public:
 	//virtual ~YeStereoCamera();
 };
 
+
+
 }
+
 
 #endif
