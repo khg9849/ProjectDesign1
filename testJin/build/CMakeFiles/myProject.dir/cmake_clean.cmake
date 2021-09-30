c6f99c7cc480b3f3ad86f1e3aef5d59db16828c2
jessie129j <jessie129j@gmail.com>
2021-09-30 22:28:56 +0900

    update2

---
+++ testJin/build/CMakeFiles/myProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProject.dir/main.cpp.o"
+  "CMakeFiles/myProject.dir/stereo.cpp.o"
+  "CMakeFiles/myProject.dir/yeStereoCamera.cpp.o"
+  "myProject"
+  "myProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
