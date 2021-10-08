f92c456db3a57a4348b42d342afeaf4050cd1333
lacoon2874 <woakd0705@naver.com>
2021-10-08 07:14:59 -0700

    feature detect and calcul z

--- testH/featureDetection.cpp
+++ testH/featureDetection.cpp
@@ -3,81 +3,64 @@
 #include <opencv2/xfeatures2d.hpp>
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
 
 int main(){
 	
 	cv::FileStorage fsr("calibration.xml", cv::FileStorage::READ);
-
+	
 	cv::Mat distCoeff_left, cameraMatrix_left;
 	cv::Mat distCoeff_right, cameraMatrix_right;
 
 	fsr["leftCameraDistCoeff"] >> distCoeff_left;
 	fsr["leftCameraMatrix"] >> cameraMatrix_left;
-	cout<<cameraMatrix_left<<'\n';
 	fsr["rightCameraDistCoeff"] >> distCoeff_right;
 	fsr["rightCameraMatrix"] >> cameraMatrix_right;
-	cout<<cameraMatrix_right<<'\n';
 
-	string path = "../resources/cube/50cm";
+	ofstream fp;
+	fp.open("40cm_right.txt");
+
+	string path = "../resources/cube/new/40cm_RIGHT.jpg";
 	vector<cv::String> images;
 	cv::glob(path, images);
 
-	cv::Ptr<cv::Feature2D> fast = cv::FastFeatureDetector::create();
-	cv::Ptr<cv::Feature2D> brief = cv::xfeatures2d::BriefDescriptorExtractor::create();
-	cv::Ptr<cv::DescriptorMatcher> matcher_orb = cv::BFMatcher::create(cv::NORM_HAMMING);
 	cv::Ptr<cv::Feature2D> orb = cv::ORB::create();
+	cv::Ptr<cv::DescriptorMatcher> matcher_orb = cv::BFMatcher::create(cv::NORM_HAMMING);
 	
-	cv::Mat img, gray, dis_pic[2], pic[2], dc[2], output, dc1[2];
-	vector<cv::KeyPoint> kp[2], kp1[2];
-	vector<vector<cv::DMatch>> match1, match2;
+	cv::Mat img, gray, pic[2], dc[2], output;
+	vector<cv::KeyPoint> kp[2];
+	vector<vector<cv::DMatch>> matches;
+
+	cv::Mat mat1, mat2;
+	mat1 = cameraMatrix_left.inv();
+	mat2 = cameraMatrix_right.inv();
 	
 	for(int i = 0; i < images.size(); i++){
 		img = cv::imread(images[i]);
 		cv::cvtColor(img, gray, cv::COLOR_RGB2GRAY);
 
-		cout<<gray.rows<<'\n'<<gray.cols<<'\n';
-		dis_pic[0] = gray(cv::Range::all(), cv::Range(0, gray.cols/2));
-		cv::undistort(dis_pic[0], pic[0], cameraMatrix_left, distCoeff_left);
-		dis_pic[1] = gray(cv::Range::all(), cv::Range(gray.cols/2, gray.cols));
-		cv::undistort(dis_pic[1], pic[1], cameraMatrix_right, distCoeff_right);
-
+		pic[0] = gray(cv::Range::all(), cv::Range(0, gray.cols/2));
+		pic[1] = gray(cv::Range::all(), cv::Range(gray.cols/2, gray.cols));
 		
 		for(int j = 0; j < 2; j++){
-			fast->detect(pic[j], kp[j]);
-			brief->compute(pic[j], kp[j], dc[j]);
-			orb->detectAndCompute(pic[j], cv::Mat(), kp1[j], dc1[j]);
+			orb->detectAndCompute(pic[j], cv::Mat(), kp[j], dc[j]);
 		}
-		matcher_orb->radiusMatch(dc[0], dc[1], match1, 10);
-		matcher_orb->radiusMatch(dc1[0], dc1[1], match2, 30);
-
-		//cv::drawMatches(pic[0], kp[0], pic[1], kp[1], match1, output, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<vector<char>>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-		//cv::imshow("1", output);
-		//cv::drawMatches(pic[0], kp1[0], pic[1], kp1[1], match2, output, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<vector<char>>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-
-		/*
-		if(match1.size()){
-			for(int k = 0; k < match2.size(); k++){
-				if(match2.at(k).size()){
-					cv::drawMatches(pic[0], kp1[0], pic[1], kp1[1], match2.at(k), output, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-					cout<<match2.at(k).at(0).queryIdx<<' '<<match2.at(k).at(0).trainIdx<<'\n';
-					//cout<<match1.at(k).size()<<'\n';
-					//cout<<match1.at(k).at(0).queryIdx<<'\n';
-					//break;
-					cv::imshow("1", output);
-					cv::waitKey(0);
+		matcher_orb->radiusMatch(dc[0], dc[1], matches, 25);
+		
+		if(matches.size()){
+			for(int k = 0; k < matches.size(); k++){
+				for(int m = 0; m < matches.at(k).size(); m++){
+					vector<cv::DMatch> temp;
+					temp.push_back(matches.at(k).at(m));
+					cv::drawMatches(pic[0], kp[0], pic[1], kp[1], temp, output, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+					fp<<"pixel difference : " << (int)kp[0].at(temp.at(0).queryIdx).pt.x-(int)kp[1].at(temp.at(0).trainIdx).pt.x << ", z value : ";
+					fp<<60/(-mat2.at<double>(0,0)*(int)(kp[1].at(temp.at(0).trainIdx).pt.x)-mat2.at<double>(0,2)+mat1.at<double>(0,0)*(int)(kp[0].at(temp.at(0).queryIdx).pt.x)+mat1.at<double>(0,2))<<'\n';
+					//cv::imshow("test", output);
+					//cv::waitKey(0);
 				}
 			}
-		}*/
-		//	if(match1.at(0).size()){
-		//		cout<<match1.at(0).at(0).queryIdx << '\n';
-		//	}
-		cv::Point2f a = kp1[0].at(72).pt, b = kp1[1].at(143).pt;
-		cout<<"a.x : " << a.x << '\n' << "a.y : " << a.y << '\n';
-		cout<<"b.x : " << b.x << '\n' << "b.y : " << b.y << '\n';
-
-		//cv::imshow("test", output);
-		//cv::waitKey(0);
+		}
 	}
 
 	return 0;
