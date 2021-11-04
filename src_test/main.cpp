3de4738e96d8d14951607f42df0dfe3c8e254775
khg9849 <hyeong12347@gmail.com>
2021-11-04 19:54:25 +0900

    test version

---
+++ src_test/main.cpp
@@ -0,0 +1,14 @@
+#include "darknet.h"
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+void callYolo(cv::Mat image);
+void display3DReCon(cv::Mat image);
+
+int main(int argc, char **argv){
+	
+	image temp = make_image(100,100,100);
+	
+
+	return 0;
+}
