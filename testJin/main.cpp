43d6bce5c73d4e1d1e9b535bdf7f5990317f068e
jessie129j@gmail.com <jessie129j@gmail.com>
2021-11-08 10:44:57 +0900

    yolov4-tiny

--- testJin/main.cpp
+++ testJin/main.cpp
@@ -14,18 +14,18 @@
 #define CHAIR 56
 #define MONITER 62
 #define CONTROLLER 67
-#define THRESHOLD 0.9
+#define THRESHOLD 0.5
 
 #define EPOCH 100
 
 //void callYolo(cv::Mat image);
 //void display3DReCon(cv::Mat image);
 
-cv::Mat cropDetection(bbox_t_container cont, int contsize, cv::Mat src, int object);
+cv::Mat cropDetection(bbox_t_container cont, int contsize,cv::Mat src, int object);
 bool saveCont(char *filepath, bbox_t_container cont, int contsize);
 bbox_t_container readCont(char *filepath, int *contsize);
 
-#define DEBUG 0
+#define DEBUG 1
 
 const String keys =
     "{help h usage ? |                  | print this message                                                }"
@@ -39,7 +39,7 @@ const String keys =
     "{no-display     |1                  | don't display results                                             }"
     "{no-downscale   |1                 | force stereo matching on full-sized views to improve quality      }"
     "{dst_conf_path  |None              | optional path to save the confidence map used in filtering        }"
-    "{vis_mult       |5.0               | coefficient used to scale disparity map visualizations            }"
+    "{vis_mult       |10.0               | coefficient used to scale disparity map visualizations            }"
     "{max_disparity  |32                | parameter of stereo matching                                      }"
     "{window_size    |-1                | parameter of stereo matching                                      }"
     "{wls_lambda     |8000.0            | parameter of wls post-filtering                                   }"
@@ -65,27 +65,18 @@ int main(int argc, char **argv)
 //	if(baseline<0) baseline*-1;
 //	cout<<"baseline is "<<baseline<<'\n';
 
-	std::string cfgpath = "../darknet/cfg/yolov4.cfg";
-	std::string weightspath = "../darknet/yolov4.weights";
-	bbox_t_container cont;
-	int contsize;
+	std::string cfgpath = "../darknet/cfg/yolov4-tiny.cfg";
+	std::string weightspath = "../darknet/yolov4-tiny.weights";
+	bbox_t_container cont1,cont2;
+	int contsize1,contsize2;
 
 	std::string photopath = "../resources/135cm/135cm_1.jpg";
 	std::string detectedpath="../resources/135cm/135cm_detected.jpg";
 	char *contpath = "../cont135.txt";
 
-	// CallYolo *yolo = new CallYolo();
-	// yolo->init(cfgpath, weightspath);
-	// yolo->setPhoto(photopath);
-	
-	// bool isContSaved = saveCont(contpath, cont, contsize);
+	CallYolo *yolo = new CallYolo();
+	yolo->init(cfgpath, weightspath);
 
-	
-	//cv::imshow("srcImg",srcImg);
-	//cv::imshow("detectedImg",detectedImg);
-	//cv::imwrite(detectedpath, detectedImg);
-	//cv::waitKey(0);
-	
 	CommandLineParser parser(argc,argv,keys);
     Display3DReCon* dp=new Display3DReCon();
     dp->init(parser);
@@ -97,16 +88,40 @@ int main(int argc, char **argv)
 	cv::Mat detectedImg;
 	double detecting_time;
 
-	cont=readCont(contpath,&contsize);
+	std::string photopath1 = "left.jpg";
+	std::string photopath2 = "right.jpg";
+	imwrite(photopath1,srcImg(cv::Rect(0,0,srcImg.cols/2,srcImg.rows)));
+	imwrite(photopath2,srcImg(cv::Rect(srcImg.cols/2,0,srcImg.cols/2,srcImg.rows)));
+
+	//bool isContSaved = saveCont(contpath, cont, contsize);
+
+	
+	// cv::imshow("srcImg",srcImg);
+	// cv::imshow("detectedImg",detectedImg);
+	// cv::imwrite(detectedpath, detectedImg);
+	// cv::waitKey(0);
+	
+	
+
+	//cont=readCont(contpath,&contsize);
 	printf("\telapsed1\tdetecting_time\telpased2\tdetecting_time+elpased2\tdiff\n");
     for(int i = 0;i < EPOCH;i++){
 		detecting_time = (double)getTickCount();
-		//cont = yolo->getCont();
-		usleep(10000); //sleep 10ms
-		detectedImg = cropDetection(cont, contsize, srcImg,CHAIR);
-		
+		yolo->setPhoto(photopath1);
+		cont1 = yolo->getCont();
+		contsize1=yolo->getContSize();
+		yolo->setPhoto(photopath2);
+		cont2 = yolo->getCont();
+		contsize2=yolo->getContSize();
 		detecting_time = ((double)getTickCount() - detecting_time)/getTickFrequency();
         
+		for(int i=0;i<contsize2;i++){
+			cont1.candidates[contsize1+i]=cont2.candidates[i];
+			cont1.candidates[contsize1+i].x+=srcImg.cols/2;
+		}
+		contsize1+=contsize2;
+
+		detectedImg = cropDetection(cont1, contsize1,srcImg,CHAIR);
         elapsed1=dp->test3dReCon(srcImg);
         elapsed2=dp->test3dReCon(detectedImg);
         diff=elapsed1-(detecting_time+elapsed2);
@@ -122,6 +137,7 @@ int main(int argc, char **argv)
 
 cv::Mat cropDetection(bbox_t_container cont, int contsize, cv::Mat src, int object)
 {
+
 	cv::Mat res = src.clone();
 	cv::Mat detected;
 	cv::Mat croptedImg[2];
@@ -138,7 +154,11 @@ cv::Mat cropDetection(bbox_t_container cont, int contsize, cv::Mat src, int obje
 		int cy = cont.candidates[i].y;
 		int cw = cont.candidates[i].w;
 		int ch = cont.candidates[i].h;
-		cv::rectangle(res, cv::Point(cx, cy), cv::Point(cx + cw, cy + ch), 1, 8, 0);
+
+		cv::Scalar color=cv::Scalar(255,0,0);
+		if(cont.candidates[i].obj_id==object)
+			color=cv::Scalar(0,0,255);
+		cv::rectangle(res, cv::Point(cx, cy), cv::Point(cx + cw, cy + ch), color, 8, 0);
 		
 #if DEBUG
 		printf("obj_id, cx, cy, cw, ch, prob = (%d, %d,%d,%d,%d,%lf)\n", cont.candidates[i].obj_id, cx, cy, cw, ch, cont.candidates[i].prob);
