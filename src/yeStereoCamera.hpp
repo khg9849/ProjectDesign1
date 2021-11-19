12a93514fbd9e4331e29e7e6ace1d7e4a901cde2
sFaster <sfaster@gmail.com>
2021-11-19 16:18:54 +0900

    Add functions in YeStereoCamera.hpp.

--- src/yeStereoCamera.hpp
+++ src/yeStereoCamera.hpp
@@ -4,13 +4,25 @@
 #include <vector>
 #include <string>
 
+#include <yolo_v2_class.hpp>
+
 namespace SYE {
+
+struct YePos3D {
+	double x;
+	double y;
+	double z;
+};
 
 class YeStereoCamera {
 private:
+	cv::Mat matCamMat1; //camera matrix.
+	cv::Mat matCamMat2; //camera matrix.
+	cv::Mat matDistCoffs1;
+	cv::Mat matDistCoffs2;
+	cv::Mat matT;			// translation matrix between two lens.
+	cv::Mat matR;			// rotation matrix between two lens.
 protected:
-	virtual bool doCalibration(const char *pPath) = 0;
-	virtual bool doCalibration(std::vector<std::string> &imgList) = 0;
 public:
 	YeStereoCamera();
 	//virtual ~YeStereoCamera();
@@ -18,6 +30,20 @@ public:
 	virtual int doTest(int i) {
 		return i * i;
 	}
+
+	//경로 내부의 이미지 파일을 읽어서 켈리브레이션 실시.
+	bool doCalibration(const char *pPath, const char *ext = ".jpg");
+	bool doCalibration(std::vector<std::string> &imgList);
+
+	// Yolo를 이용하여 특정 이름의 영역을 추출.
+	bool findImage(const cv::Mat mat, const char *objName, bbox_t *pObjRect);
+
+	//Obsolute length from camera.
+	bool getObsoluteLengthInRect(const cv::Mat src, bbox_t *pObjRect, std::vector<YePos3D> &features);
+
+
+	// 추춘된 특정 영역만 SGBM 3D reconstruction.
+	bool getSgbmInRect(const cv::Mat src, bbox_t *pObject, cv::Mat rtn);
 };
 
 }
