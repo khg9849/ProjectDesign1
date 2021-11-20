f393e40b5eadb7e6d52fc5b11b06e76f6c51a754
khg9849 <75154470+khg9849@users.noreply.github.com>
2021-11-20 15:24:49 +0900

    make cpp let's go develop

--- src/yeStereoCamera.cpp
+++ src/yeStereoCamera.cpp
@@ -7,3 +7,36 @@ using namespace SYE;
 YeStereoCamera::YeStereoCamera() {
 	std::cout << "Create class" << std::endl;
 }
+YeStereoCamera::~YeStereoCamera() {
+}
+
+//경로 내부의 이미지 파일을 읽어서 켈리브레이션 실시.
+bool YeStereoCamera::doCalibration(const char* pPath, const char* ext = ".jpg") {
+
+}
+bool YeStereoCamera::doCalibration(std::vector<std::string>& imgList) {
+
+}
+
+
+
+// Yolo를 이용하여 특정 이름의 영역을 추출.
+bool YeStereoCamera::findImage(const cv::Mat mat, const char* objName, bbox_t* pObjRect) 
+{
+
+}
+
+
+
+//Absolute length from camera.
+bool YeStereoCamera::getAbsoluteLengthInRect(const cv::Mat src, bbox_t* pObjRect, std::vector<YePos3D>& features) {
+
+}
+
+
+// 추춘된 특정 영역만 SGBM 3D reconstruction.
+bool YeStereoCamera::getSgbmInRect(const cv::Mat src, bbox_t* pObject, cv::Mat rtn) {
+
+}
+
+/*--------------------------------------------------4 팀 화 이 팅 ! ! ! ,,----------------------------------------------------*/
--- src/yeStereoCamera.hpp
+++ src/yeStereoCamera.hpp
@@ -25,11 +25,11 @@ private:
 protected:
 public:
 	YeStereoCamera();
-	//virtual ~YeStereoCamera();
-
+	~YeStereoCamera();
+	/*
 	virtual int doTest(int i) {
 		return i * i;
-	}
+	}*/
 
 	//경로 내부의 이미지 파일을 읽어서 켈리브레이션 실시.
 	bool doCalibration(const char *pPath, const char *ext = ".jpg");
@@ -38,8 +38,8 @@ public:
 	// Yolo를 이용하여 특정 이름의 영역을 추출.
 	bool findImage(const cv::Mat mat, const char *objName, bbox_t *pObjRect);
 
-	//Obsolute length from camera.
-	bool getObsoluteLengthInRect(const cv::Mat src, bbox_t *pObjRect, std::vector<YePos3D> &features);
+	//Absolute length from camera.
+	bool getAbsoluteLengthInRect(const cv::Mat src, bbox_t *pObjRect, std::vector<YePos3D> &features);
 
 
 	// 추춘된 특정 영역만 SGBM 3D reconstruction.
